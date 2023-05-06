ec0089b57b22a50c7868c59096b190ba84e6cdc8
NIVAS BODAPATI <nivasbodapati.abc@gmail.com>
2023-05-06 06:57:27 +0530

    Added all the tree details

--- src/data.js
+++ src/data.js
@@ -1,16 +1,87 @@
 import tree1 from './assets/1.jpeg';
 import audio1 from './assets/neredu.mp4';
-
+import tree2 from './assets/2.jpeg';
+import audio2 from './assets/Nauclia.unknown';
 import tree3 from './assets/3.jpeg';
 import audio3 from './assets/silky oak.mp4';
 import tree4 from './assets/4.jpeg';
 import audio4 from './assets/thati.mp4';
-
+import tree5 from './assets/5.jpeg';
+import audio5 from './assets/plumeria.unknown';
 import tree7 from './assets/7.jpeg';
 import audio7 from './assets/neredu.mp4';
-
+import tree8 from './assets/8.jpeg';
+import audio8 from './assets/Nauclia.unknown';
 import tree9 from './assets/9.jpeg';
 import audio9 from './assets/jack tree.mp4';
+import tree10 from './assets/10.jpeg';
+import audio10 from './assets/Terminalia katappa.unknown';
+import tree11 from './assets/11.jpeg';
+import audio11 from './assets/African tulip.unknown';
+import tree12 from './assets/12.jpeg';
+import audio12 from './assets/neredu.mp4';
+import tree13 from './assets/13.jpeg';
+import audio13 from './assets/Neem.unknown';
+import tree14 from './assets/14.jpeg';
+import audio14 from './assets/Eucalyptus.unknown';
+import tree15 from './assets/15.jpeg';
+import audio15 from './assets/Kanuga.unknown';
+import tree16 from './assets/16.jpeg';
+import audio16 from './assets/neredu.mp4';
+import tree17 from './assets/17.jpeg';
+import audio17 from './assets/neredu.mp4';
+import tree18 from './assets/18.jpeg';
+import audio18 from './assets/Terminalia katappa.unknown';
+import tree19 from './assets/19.jpeg';
+import audio19 from './assets/adavi chinta.mp4';
+import tree20 from './assets/20.jpeg';
+import audio20 from './assets/pogada.unknown';
+import tree21 from './assets/21.jpeg';
+import audio21 from './assets/Eucalyptus Nicholli.unknown';
+import tree22 from './assets/22.jpeg';
+import audio22 from './assets/Nauclia.unknown';
+import tree23 from './assets/23.jpeg';
+import audio23 from './assets/Syagrus romanzaoffiana.unknown';
+import tree24 from './assets/24.jpeg';
+import audio24 from './assets/neredu.mp4';
+import tree25 from './assets/25.jpeg';
+import audio25 from './assets/Eucalyptus Nicholli.unknown';
+import tree26 from './assets/26.jpeg';
+import audio26 from './assets/pogada.unknown';
+import tree27 from './assets/27.jpeg';
+import audio27 from './assets/Kanuga.unknown';
+import tree28 from './assets/28.jpeg';
+import audio28 from './assets/Kanuga.unknown';
+import tree29 from './assets/29.jpeg';
+import audio29 from './assets/Kanuga.unknown';
+import tree30 from './assets/30.jpeg';
+import audio30 from './assets/Afzelia Xylocarpa.unknown';
+import tree32 from './assets/32.jpeg';
+import audio32 from './assets/Kanuga.unknown';
+import tree33 from './assets/33.jpeg';
+import audio33 from './assets/Eucalyptus.unknown';
+import tree34 from './assets/34.jpeg';
+import audio34 from './assets/Neem.unknown';
+import tree35 from './assets/35.jpeg';
+import audio35 from './assets/adavi chinta.mp4';
+import tree36 from './assets/36.jpeg';
+import audio36 from './assets/plumeria.unknown';
+import tree38 from './assets/38.jpeg';
+import audio38 from './assets/Neem.unknown';
+import tree39 from './assets/39.jpeg';
+import audio39 from './assets/Bamboo.unknown';
+import tree40 from './assets/40.jpeg';
+import audio40 from './assets/neredu.mp4';
+import tree41 from './assets/41.jpeg';
+import audio41 from './assets/mango.mp4';
+import tree42 from './assets/42.jpeg';
+import audio42 from './assets/neredu.mp4';
+import tree43 from './assets/43.jpeg';
+import audio43 from './assets/pogada.unknown';
+import tree44 from './assets/44.jpeg';
+import audio44 from './assets/adavi chinta.mp4';
+import tree45 from './assets/45.jpeg';
+import audio45 from './assets/adavi chinta.mp4';
 
 
 
@@ -27,9 +98,20 @@ const Array = [
         "Latitude": 16.980848,
         "Longitude": 82.240676,
         "Plantation Year": 2009,
-        "Oxygen Released per year": "69.35 liters"
+        "Oxygen Released": "69.35 liters"
     },
-    
+    {
+        "Audio": audio2,
+        "ImageURL": tree2,
+        "Name": "Nauclia(Kadambakamu)",
+        "Area": "1.3 sq meter",
+        "Age": "12 years",
+        "Height": "9 meters",
+        "Latitude": 16.980799,
+        "Longitude": 82.240228,
+        "Plantation Year": 2011,
+        "Oxygen Released": "62.05 liters"
+    },
     {
         "Audio": audio3,
         "ImageURL": tree3,
@@ -40,7 +122,7 @@ const Array = [
         "Latitude": 16.98093,
         "Longitude": 82.240028,
         "Plantation Year": 2007,
-        "Oxygen Released per year": "73 liters"
+        "Oxygen Released": "73 liters"
     },
     {
         "Audio": audio4,
@@ -52,9 +134,20 @@ const Array = [
         "Latitude": 16.980639,
         "Longitude": 82.24001,
         "Plantation Year": 2011,
-        "Oxygen Released per year": "43.8 liters"
+        "Oxygen Released": "43.8 liters"
     },
-    
+    {
+        "Audio": audio5,
+        "ImageURL": tree5,
+        "Name": "Plumeria(Frangipani)",
+        "Area": "0.9 sq meter",
+        "Age": "6 years",
+        "Height": "5 meters",
+        "Latitude": 16.980982,
+        "Longitude": 82.2404,
+        "Plantation Year": 2017,
+        "Oxygen Released": "32.85 liters"
+    },
     {"Audio": audio7,
         "ImageURL": tree7,
         "Name": "Syzygium Cumini(Neredu)",
@@ -64,9 +157,18 @@ const Array = [
         "Latitude": 16.980934,
         "Longitude": 82.240021,
         "Plantation Year": 2009,
-        "Oxygen Released per year": "69.35 liters"},
+        "Oxygen Released": "69.35 liters"},
 
-       
+        {"Audio": audio8,
+        "ImageURL": tree8,
+        "Name": "Nauclia(Kadambakamu)",
+        "Area": "0.9 sq meter",
+        "Age": "9 years",
+        "Height": "7 meters",
+        "Latitude": 16.980662,
+        "Longitude": 82.240353,
+        "Plantation Year": 2014,
+        "Oxygen Released": "62.05 liters"},
 
         {"Audio": audio9,
         "ImageURL": tree9,
@@ -77,7 +179,385 @@ const Array = [
         "Latitude": 16.980545,
         "Longitude": 82.240401,
         "Plantation Year": 2012,
-        "Oxygen Released per year": "69.35 liters"}
+        "Oxygen Released": "69.35 liters"},
+
+        {"Audio": audio10,
+        "ImageURL": tree10,
+        "Name": "Terminalia Kattappa(Tropical Almond)",
+        "Area": "2.4 sq meter",
+        "Age": "15 years",
+        "Height": "9 meters",
+        "Latitude": 16.980361,
+        "Longitude": 82.240748,
+        "Plantation Year": 2008,
+        "Oxygen Released": "91.25 liters"},
+
+
+        
+        {"Audio": audio11,
+        "ImageURL": tree11,
+        "Name": "African tulip(Spathodea Campanulata)",
+        "Area": "3 sq meter",
+        "Age": "13 years",
+        "Height": "8 meters",
+        "Latitude": 16.980484,
+        "Longitude": 82.240581,
+        "Plantation Year": 2010,
+        "Oxygen Released": "69.35 liters"},
+
+        
+        {"Audio": audio12,
+        "ImageURL": tree12,
+        "Name": "Syzygium(Neredu)",
+        "Area": "2.1 sq meter",
+        "Age": "9 years",
+        "Height": "10 meters",
+        "Latitude": 16.98029,
+        "Longitude": 82.240639,
+        "Plantation Year": 2014,
+        "Oxygen Released": "95 liters"},
+
+        
+        {"Audio": audio13,
+        "ImageURL": tree13,
+        "Name": "Neem Tree(Vepa)",
+        "Area": "1.0 sq meter",
+        "Age": "20 years",
+        "Height": "18 meters",
+        "Latitude": 16.980223,
+        "Longitude": 82.240684,
+        "Plantation Year": 2003,
+        "Oxygen Released": "76.65 liters"},
+
+        {"Audio": audio14,
+        "ImageURL": tree14,
+        "Name": "Eucalyptus(Neelagiri)",
+        "Area": "1.3 sq meter",
+        "Age": "25 years",
+        "Height": "20 meters",
+        "Latitude": 16.979965,
+        "Longitude": 82.240309,
+        "Plantation Year": 1998,
+        "Oxygen Released": "94.9 liters"},
+
+        {"Audio": audio15,
+        "ImageURL": tree15,
+        "Name": "Bauhinia Purpuria(Kanuga)",
+        "Area": "0.8 sq meter",
+        "Age": "13 years",
+        "Height": "9 meters",
+        "Latitude": 16.979874,
+        "Longitude": 82.240182,
+        "Plantation Year": 2010,
+        "Oxygen Released": "73.4 liters"},
+
+        {"Audio": audio16,
+        "ImageURL": tree16,
+        "Name": "Syzygium Cumini(Neredu)",
+        "Area": "1.0 sq meter",
+        "Age": "16 years",
+        "Height": "13 meters",
+        "Latitude": 16.980127,
+        "Longitude": 82.240037,
+        "Plantation Year": 2007,
+        "Oxygen Released": "83.7 liters"},
+
+        {"Audio": audio17,
+        "ImageURL": tree17,
+        "Name": "Syzygium Cumini(Neredu)",
+        "Area": "1.0 sq meter",
+        "Age": "13 years",
+        "Height": "9 meters",
+        "Latitude": 16.980165,
+        "Longitude": 82.239997,
+        "Plantation Year": 2010,
+        "Oxygen Released": "81.25 liters"},
+
+        {"Audio": audio18,
+        "ImageURL": tree18,
+        "Name": "Terminalia Kattappa(Tropical Almond)",
+        "Area": "2.0 sq meter",
+        "Age": "17 years",
+        "Height": "10 meters",
+        "Latitude": 16.980353,
+        "Longitude": 82.239963,
+        "Plantation Year": 2006,
+        "Oxygen Released": "43.0 liters"},
+
+        {"Audio": audio19,
+        "ImageURL": tree19,
+        "Name": "Pelpophorum Pterocarpum(Adavi Chinta)",
+        "Area": "2.0 sq meter",
+        "Age": "19 years",
+        "Height": "15 meters",
+        "Latitude": 16.979858,
+        "Longitude": 82.23969,
+        "Plantation Year": 2004,
+        "Oxygen Released": "89.35 liters"},
+
+        {"Audio": audio20,
+        "ImageURL": tree20,
+        "Name": "Pogada(Minusops Elengi Linn)",
+        "Area": "1.0 sq meter",
+        "Age": "9 years",
+        "Height": "7 meters",
+        "Latitude": 16.980016,
+        "Longitude": 82.241496,
+        "Plantation Year": 2014,
+        "Oxygen Released": "52.5 liters"},
+
+        {"Audio": audio21,
+        "ImageURL": tree21,
+        "Name": "Narrow Leaf Peppermint(eucalyptus nicholli)",
+        "Area": "2.1 sq meter",
+        "Age": "14 years",
+        "Height": "16 meters",
+        "Latitude": 16.979879,
+        "Longitude": 82.24146,
+        "Plantation Year": 2009,
+        "Oxygen Released": "88.5 liters"},
+
+        {"Audio": audio22,
+        "ImageURL": tree22,
+        "Name": "Nauclia(Kadambakamu)",
+        "Area": "2.0 sq meter",
+        "Age": "10 years",
+        "Height": "12 meters",
+        "Latitude": 16.979846,
+        "Longitude": 82.241368,
+        "Plantation Year": 2013,
+        "Oxygen Released": "67.5 liters"},
+
+        {"Audio": audio23,
+        "ImageURL": tree23,
+        "Name": "Syagrus romanzaoffiana",
+        "Area": "1.0 sq meter",
+        "Age": "8 years",
+        "Height": "10 meters",
+        "Latitude": 16.979729,
+        "Longitude": 82.241305,
+        "Plantation Year": 2015,
+        "Oxygen Released": "69 liters"},
+
+        {"Audio": audio24,
+        "ImageURL": tree24,
+        "Name": "Syzygium(Neredu)",
+        "Area": "2.0 sq meter",
+        "Age": "16 years",
+        "Height": "13 meters",
+        "Latitude": 16.979622,
+        "Longitude": 82.241302,
+        "Plantation Year": 2007,
+        "Oxygen Released": "78 liters"},
+
+        {"Audio": audio25,
+        "ImageURL": tree25,
+        "Name": "Narrow Leaf Peppermint(eucalyptus nicholli)",
+        "Area": "2.4 sq meter",
+        "Age": "19 years",
+        "Height": "19 meters",
+        "Latitude": 16.979487,
+        "Longitude": 82.241219,
+        "Plantation Year": 2004,
+        "Oxygen Released": "80 liters"},
+
+        {"Audio": audio26,
+        "ImageURL": tree26,
+        "Name": "Pogada(Minusops Elengi Linn)",
+        "Area": "1.0 sq meter",
+        "Age": "9 years",
+        "Height": "11 meters",
+        "Latitude": 16.978961,
+        "Longitude": 82.24144,
+        "Plantation Year": 2014,
+        "Oxygen Released": "52.5 liters"},
+
+        {"Audio": audio27,
+        "ImageURL": tree27,
+        "Name": "Bauhinia Purpuria(Kanuga)",
+        "Area": "2.0 sq meter",
+        "Age": "12 years",
+        "Height": "11 meters",
+        "Latitude": 16.97779,
+        "Longitude": 82.242194,
+        "Plantation Year": 2011,
+        "Oxygen Released": "65.25 liters"},
+
+        {"Audio": audio28,
+        "ImageURL": tree28,
+        "Name": "Bauhinia Purpuria(Kanuga)",
+        "Area": "2.0 sq meter",
+        "Age": "12 years",
+        "Height": "10 meters",
+        "Latitude": 16.977942,
+        "Longitude": 82.242124,
+        "Plantation Year": 2011,
+        "Oxygen Released": "62.5 liters"},
+
+        {"Audio": audio29,
+        "ImageURL": tree29,
+        "Name": "Bauhinia Purpuria(Kanuga)",
+        "Area": "2.0 sq meter",
+        "Age": "12 years",
+        "Height": "10 meters",
+        "Latitude": 16.978134,
+        "Longitude": 82.2421,
+        "Plantation Year": 2011,
+        "Oxygen Released": "62.5 liters"},
+
+        {"Audio": audio30,
+        "ImageURL": tree30,
+        "Name": "Afzelia Xylocarpa",
+        "Area": "2.8 sq meter",
+        "Age": "26 years",
+        "Height": "24 meters",
+        "Latitude": 16.978414,
+        "Longitude": 82.241715,
+        "Plantation Year": 1997,
+        "Oxygen Released": "96.75 liters"},
+
+        {"Audio": audio32,
+        "ImageURL": tree32,
+        "Name": "Bauhinia Purpuria(Kanuga)",
+        "Area": "1.8 sq meter",
+        "Age": "14 years",
+        "Height": "23 meters",
+        "Latitude": 16.978408,
+        "Longitude": 82.241712,
+        "Plantation Year": 2009,
+        "Oxygen Released": "82.5 liters"},
+
+        {"Audio": audio33,
+        "ImageURL": tree33,
+        "Name": "Eucalyptus(Neelagiri)",
+        "Area": "1.5 sq meter",
+        "Age": "15 years",
+        "Height": "20 meters",
+        "Latitude": 16.977996,
+        "Longitude": 82.241456,
+        "Plantation Year": 2008,
+        "Oxygen Released": "75.0 liters"},
+
+        {"Audio": audio34,
+        "ImageURL": tree34,
+        "Name": "Neem Tree(Vepa)",
+        "Area": "2.1 sq meter",
+        "Age": "14 years",
+        "Height": "14 meters",
+        "Latitude": 16.97813,
+        "Longitude": 82.241402,
+        "Plantation Year": 2009,
+        "Oxygen Released": "65.0 liters"},
+
+        {"Audio": audio35,
+        "ImageURL": tree35,
+        "Name": "Pelpophorum Pterocarpum(Adavi Chinta)",
+        "Area": "1.0 sq meter",
+        "Age": "9 years",
+        "Height": "11 meters",
+        "Latitude": 16.978961,
+        "Longitude": 82.24144,
+        "Plantation Year": 2014,
+        "Oxygen Released": "52.5 liters"},
+
+        {"Audio": audio36,
+        "ImageURL": tree36,
+        "Name": "Plumeria(Frangipani)",
+        "Area": "0.8 sq meter",
+        "Age": "8 years",
+        "Height": "7 meters",
+        "Latitude": 16.978828,
+        "Longitude": 82.240377,
+        "Plantation Year": 2015,
+        "Oxygen Released": "42.5 liters"},
+
+        {"Audio": audio38,
+        "ImageURL": tree38,
+        "Name": "Neem Tree(Vepa), Bamboo Tree",
+        "Area": "1.5 sq meter",
+        "Age": "12 years",
+        "Height": "17, 12 meters",
+        "Latitude": 16.979004,
+        "Longitude": 82.24021,
+        "Plantation Year": 2011,
+        "Oxygen Released": "65 liters"},
+
+        {"Audio": audio39,
+        "ImageURL": tree39,
+        "Name": "Bamboo Tree",
+        "Area": "1.2 sq meter",
+        "Age": "13 years",
+        "Height": "15 meters",
+        "Latitude": 16.979022,
+        "Longitude": 82.240235,
+        "Plantation Year": 2010,
+        "Oxygen Released": "67 liters"},
+
+        {"Audio": audio40,
+        "ImageURL": tree40,
+        "Name": "Syzygium Cumini(Neredu)",
+        "Area": "2.0 sq meter",
+        "Age": "19 years",
+        "Height": "17 meters",
+        "Latitude": 16.979045,
+        "Longitude": 82.240242,
+        "Plantation Year": 2004,
+        "Oxygen Released": "92.5 liters"},
+
+        {"Audio": audio41,
+        "ImageURL": tree41,
+        "Name": "Mango(Mangifera indica)",
+        "Area": "2.0 sq meter",
+        "Age": "18 years",
+        "Plantation Year": 2005,
+        "Oxygen Released": "75.0 liters"},
+
+        {"Audio": audio42,
+        "ImageURL": tree42,
+        "Name": "Syzygium Cumini(Neredu)",
+        "Area": "1.0 sq meter",
+        "Age": "16 years",
+        "Height": "18 meters",
+        "Latitude": 16.97924,
+        "Longitude": 82.240408,
+        "Plantation Year": 2007,
+        "Oxygen Released": "72 liters"},
+
+        
+        {"Audio": audio43,
+        "ImageURL": tree43,
+        "Name": "Pogada(Minusops Elengi Linn)",
+        "Area": "1.0 sq meter",
+        "Age": "7 years",
+        "Height": "9 meters",
+        "Latitude": 16.979882,
+        "Longitude": 82.240048,
+        "Plantation Year": 2007,
+        "Oxygen Released": "38.5 liters"},
+
+        {"Audio": audio44,
+        "ImageURL": tree44,
+        "Name": "Peltophorum Petrocarpum(Adavi chinta)",
+        "Area": "2.0 sq meter",
+        "Age": "22 years",
+        "Height": "24 meters",
+        "Latitude": 16.980663,
+        "Longitude": 82.24228,
+        "Plantation Year": "2001",
+        "Oxygen Released": "88.5 liters"},
+
+        {"Audio": audio45,
+        "ImageURL": tree45,
+        "Name": "Peltophorum Petrocarpum(Adavi Chintha)",
+        "Area": "1.6 sq meter",
+        "Age": "14 years",
+        "Height": "18 meters",
+        "Latitude": 16.980522,
+        "Longitude": 82.242233,
+        "Plantation Year": 2009,
+        "Oxygen Released": "65.0 liters"},
+
+        
         
 ]
 
---
+++ src/data3.js
@@ -0,0 +1,179 @@
+import tree1 from './assets/1.jpeg';
+import audio1 from './assets/neredu.mp4';
+import tree2 from './assets/2.jpeg';
+import audio2 from './assets/';
+import tree3 from './assets/3.jpeg';
+import audio3 from './assets/silky oak.mp4';
+import tree4 from './assets/4.jpeg';
+import audio4 from './assets/thati.mp4';
+import tree5 from './assets/5.jpeg';
+import audio5 from './assets/';
+import tree7 from './assets/7.jpeg';
+import audio7 from './assets/neredu.mp4';
+import tree8 from './assets/8.jpeg';
+import audio8 from './assets/';
+import tree9 from './assets/9.jpeg';
+import audio9 from './assets/jack tree.mp4';
+import tree9 from './assets/6.jpeg';
+import audio9 from './assets/audio1.aac';
+
+
+// import Audio from './assets/--audio-name---';
+
+const Array = [
+    {
+        "Audio": audio1,
+        "ImageURL": tree1,
+        "Name": "Syzygium Cumini(Neredu)",
+        "Area": "1.2 sq meter",
+        "Age": "14 years",
+        "Height": "13 meters",
+        "Latitude": 16.980848,
+        "Longitude": 82.240676,
+        "Plantation Year": 2009,
+        "Oxygen Released per year": "69.35 liters"
+    },
+    {
+        "Audio": audio2,
+        "ImageURL": tree2,
+        "Name": "Nauclia(Kadambakamu)",
+        "Area": "1.3 sq meter",
+        "Age": "12 years",
+        "Height": "9 meters",
+        "Latitude": 16.980799,
+        "Longitude": 82.240228,
+        "Plantation Year": 2011,
+        "Oxygen Released per year": "62.05 liters"
+    },
+    {
+        "Audio": audio3,
+        "ImageURL": tree3,
+        "Name": "Southern Silky Oak(Grevillea robust)",
+        "Area": "0.9 sq meter",
+        "Age": "16 years",
+        "Height": "14.5 meters",
+        "Latitude": 16.98093,
+        "Longitude": 82.240028,
+        "Plantation Year": 2007,
+        "Oxygen Released per year": "73 liters"
+    },
+    {
+        "Audio": audio4,
+        "ImageURL": tree4,
+        "Name": "Red Latin Palm(Thati)",
+        "Area": "2.0 sq meter",
+        "Age": "12 years",
+        "Height": "4 meters",
+        "Latitude": 16.980639,
+        "Longitude": 82.24001,
+        "Plantation Year": 2011,
+        "Oxygen Released per year": "43.8 liters"
+    },
+    {
+        "Audio": audio5,
+        "ImageURL": tree5,
+        "Name": "Plumeria(Frangipani)",
+        "Area": "0.9 sq meter",
+        "Age": "6 years",
+        "Height": "5 meters",
+        "Latitude": 16.980982,
+        "Longitude": 82.2404,
+        "Plantation Year": 2017,
+        "Oxygen Released per year": "32.85 liters"
+    },
+    {"Audio": audio7,
+        "ImageURL": tree7,
+        "Name": "Syzygium Cumini(Neredu)",
+        "Area": "1.2 sq meter",
+        "Age": "14 years",
+        "Height": "12 meters",
+        "Latitude": 16.980934,
+        "Longitude": 82.240021,
+        "Plantation Year": 2009,
+        "Oxygen Released per year": "69.35 liters"},
+
+        {"Audio": audio8,
+        "ImageURL": tree8,
+        "Name": "Nauclia(Kadambakamu)",
+        "Area": "0.9 sq meter",
+        "Age": "9 years",
+        "Height": "7 meters",
+        "Latitude": 16.980662,
+        "Longitude": 82.240353,
+        "Plantation Year": 2014,
+        "Oxygen Released per year": "62.05 liters"},
+
+        {"Audio": audio9,
+        "ImageURL": tree9,
+        "Name": "Jack Fruit Tree(Panasa)",
+        "Area": "0.72 sq meter",
+        "Age": "11 years",
+        "Height": "6 meters",
+        "Latitude": 16.980545,
+        "Longitude": 82.240401,
+        "Plantation Year": 2012,
+        "Oxygen Released per year": "69.35 liters"},
+
+        {"Audio": audio10,
+        "ImageURL": tree10,
+        "Name": "Terminalia Kattappa(Tropical Almond)",
+        "Area": "2.4 sq meter",
+        "Age": "15 years",
+        "Height": "9 meters",
+        "Latitude": 16.980361,
+        "Longitude": 82.240748,
+        "Plantation Year": 2008,
+        "Oxygen Released per year": "91.25 liters"},
+
+
+        {"Audio": audio10,
+        "ImageURL": tree10,
+        "Name": "Terminalia Kattappa(Tropical Almond)",
+        "Area": "2.4 sq meter",
+        "Age": "15 years",
+        "Height": "9 meters",
+        "Latitude": 16.980361,
+        "Longitude": 82.240748,
+        "Plantation Year": 2008,
+        "Oxygen Released per year": "91.25 liters"},
+
+        
+        {"Audio": audio11,
+        "ImageURL": tree11,
+        "Name": "African tulip(Spathodea Campanulata",
+        "Area": "3 sq meter",
+        "Age": "13 years",
+        "Height": "8 meters",
+        "Latitude": 16.980484,
+        "Longitude": 82.240581,
+        "Plantation Year": 2010,
+        "Oxygen Released per year": "69.35 liters"},
+
+        
+        {"Audio": audio12,
+        "ImageURL": tree12,
+        "Name": "Syzygium(Neredu)",
+        "Area": "2.1 sq meter",
+        "Age": "9 years",
+        "Height": "10 meters",
+        "Latitude": 16.98029,
+        "Longitude": 82.240639,
+        "Plantation Year": 2014,
+        "Oxygen Released per year": "95 liters"},
+
+        
+        {"Audio": audio13,
+        "ImageURL": tree13,
+        "Name": "",
+        "Area": "2.4 sq meter",
+        "Age": "15 years",
+        "Height": "9 meters",
+        "Latitude": 16.980361,
+        "Longitude": 82.240748,
+        "Plantation Year": 2008,
+        "Oxygen Released per year": "91.25 liters"},
+
+        
+]
+
+export default Array;
